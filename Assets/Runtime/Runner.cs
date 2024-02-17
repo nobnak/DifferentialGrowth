b2f7adc5fc22cf20330f888061755c35f4531d63
Nakata Nobuyuki <nobu.nakata@gmail.com>
2024-02-18 02:51:57 +0900

    backup

--- Assets/Runtime/Runner.cs
+++ Assets/Runtime/Runner.cs
@@ -10,6 +10,8 @@ using UnityEngine.Rendering;
 namespace DiffentialGrowth {
 
     public class Runner : MonoBehaviour {
+        [SerializeField]
+        protected Tuner tuner = new();
 
         protected GraphicsList<Particle> particles;
         protected GLMaterial gl;
@@ -21,6 +23,8 @@ namespace DiffentialGrowth {
                 var buf = new GraphicsBuffer(GraphicsBuffer.Target.Structured, size, Marshal.SizeOf<Particle>());
                 return buf;
             });
+
+            InitParticles();
         }
         void OnDisable() {
             gl?.Dispose();
@@ -58,12 +62,88 @@ namespace DiffentialGrowth {
                 }
             }
         }
+        void Update() {
+            var dist_insert = tuner.baseDist;
+            var dist_repulse = dist_insert * tuner.repulsion_dist;
+
+            var dt = 1f / 30; // Time.deltaTime;
+            var eps = dist_insert * EPSILON;
+
+            for (var i = 0; i < particles.Count; i++) {
+                var p = particles[i];
+                float2 accel = default;
+                var n_neighboars = 0;
+                for (var j = 0; j < particles.Count; j++) {
+                    if (i == j) continue;
+                    var q = particles[j];
+                    var dx = q.position - p.position;
+                    var dist_sq = math.lengthsq(dx);
+                    if (eps < dist_sq && dist_sq < dist_repulse * dist_repulse) {
+                        var dist = math.sqrt(dist_sq);
+                        var f = dx / (dist_sq * dist);
+                        accel -= f * 0.1f;
+                        n_neighboars++;
+                    }
+                }
+
+                if (n_neighboars > 0)
+                    p.velocity += accel * (dt / n_neighboars);
+                particles[i] = p;
+            }
+
+            var dumper = math.saturate(tuner.dump_velocity * dt);
+            for (var i = 0; i < particles.Count; i++) {
+                var p = particles[i];
+                p.position += p.velocity * dt;
+                p.velocity = math.lerp(p.velocity, 0, dumper);
+                particles[i] = p;
+            }
+
+            for (var i = 0; i < particles.Count; i++) {
+                var p0 = particles[i];
+                var p1 = particles[(i + 1) % particles.Count];
+                var dist_sq = math.distancesq(p0.position, p1.position);
+                if (dist_sq > dist_insert * dist_insert) {
+                    var p = new Particle() {
+                        position = (p0.position + p1.position) / 2,
+                        velocity = p1.position - p0.position,
+                    };
+                    particles.Insert(i + 1, p);
+                    i++;
+                }
+            }
+        }
+        #endregion
+
+        #region methods
+        private void InitParticles() {
+            particles.Clear();
+            var n = 10;
+            var r = 1f;
+            for (var i = 0; i < n; i++) {
+                var a = i * (2 * math.PI / n);
+                var p = new Particle() {
+                    position = new float2(math.cos(a) * r, math.sin(a) * r),
+                    velocity = new float2(0, 0),
+                };
+                particles.Add(p);
+            }
+        }
         #endregion
 
         #region declarations
+        public const float EPSILON = 1e-2f;
+
         public struct Particle {
             public float2 position;
             public float2 velocity;
+        }
+
+        [System.Serializable]
+        public class Tuner {
+            public float baseDist = 1f;
+            public float repulsion_dist = 5f;
+            public float dump_velocity = 10f;
         }
         #endregion
     }
